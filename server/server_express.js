906a3ca979fd6119ca61ec3ddcc5ed557f68a145
eduart <abdullahu.eduart@outlook.com>
2022-12-06 15:44:38 +0100

    setting up the server

---
+++ server/server_express.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express ()
+var path = require("path");
+
+app.set('view engine', 'ejs');
+
+app.set('public', path.join(__dirname, '../static/css'));
+app.set('views', path.join(__dirname, '../static/templates'));
+app.use(express.static(path.join(__dirname, '../static')));
+app.use("/css",express.static(path.join(__dirname, "'../static/css")))
+
+
+app.get('/', function(req,res,next){
+    res.render('home_page.ejs');
+});
+
+app.get('/login', function(req,res,next){
+    res.render('login.ejs');
+});
+
+app.get('/admin/add_movie', function(req,res,next){
+    res.render('add_movie.ejs');
+});
+app.get('/admin/modify_movie', function(req,res,next){
+    res.render('modify_movie.ejs');
+});
+app.get('/reservation', function(req,res,next){
+    res.render('reservation.ejs');
+});
+
+app.listen(8080);
