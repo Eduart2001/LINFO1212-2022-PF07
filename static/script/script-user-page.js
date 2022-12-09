e12693a0f03f03b0c43770b128e87912747442d5
Adham-Elzawawi <adham.elzawawi@student.uclouvain.be>
2022-12-09 12:54:17 +0100

    changes

--- static/script/script-user-page.js
+++ static/script/script-user-page.js
@@ -1,19 +1,37 @@
-const sequelize=require("../../server/server_express").sequelize
-const User=require("../../server/server_express").User
-var app=require("../../server/server_express").app
-var crypto=require("crypto")
+let submit=document.getElementById("clicked");
 
+function change_password(old_pass,new_pass,new_pass_rep){
 
-function change_password(old_pass,new_pass){
-    const check= sequelize.query('Select password from User');
-    if(old_pass===new_pass){
-        ///check if new pass=old pass
+    
+    if(old_pass.value===new_pass.value){
+        alert('old password is the same as new password');
+    
     }
-    else if (check===old_pass){
-        ///set new pass
-        User.password=new_pass;
+    else if(new_pass.value!==new_pass_rep.value){
+        alert(new_pass+" "+new_pass_rep);
     }
-    else{
-        throw new console.error('wrong password given');
-    }
+    else{alert("hi");}
+    
+
+    //add part to change password
+}
+
+function email(){
+    const mail=document.getElementById("email");
+    //change email
+}
+
+function first_name(){
+    const fname=document.getElementById("fname");
+    //change first name
+}
+
+function last_name(){
+    const lname=document.getElementById("fname");
+    //change last name
+}
+
+function birthday(){
+    const bday=document.getElementById("bday");
+    //change birthday
 }
