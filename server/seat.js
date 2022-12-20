a768a06d760ca32af3a102953991a063734cf1a7
eduart <abdullahu.eduart@outlook.com>
2022-12-20 14:42:33 +0100

    update reservation

---
+++ server/seat.js
@@ -0,0 +1,7 @@
+const sequelize = require("./server_express").sequelize;
+const Seat = require("./server_express").Seat;
+
+
+async function getReservedSeatsForTimeTable(timeTable){
+    return 0;
+}
