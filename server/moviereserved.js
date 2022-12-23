3caeeb1c27e0d6fb6c1a2009591ea286f5c6c40b
Adham-Elzawawi <104617248+Adham-Elzawawi@users.noreply.github.com>
2022-12-23 01:22:45 +0100

    ...

--- server/moviereserved.js
+++ server/moviereserved.js
@@ -1,4 +1,5 @@
 var express=require('express');
+const { getMaxListeners } = require('process');
 const MovieReserved = require('../Database/MovieReserved');
 const { sequelize } = require('./server_express');
 app=require("./server_express").app;
@@ -18,11 +19,10 @@ async function AllMovieUser(email){
 async function emptyReservationDB(){
     const [result,meta]= await sequelize.query('SELECT * FROM MovieReserveds WHERE ID IS NOT NULL');
     if(await result.length>0){
-        console.log("There are movies are reserved");
-        return "There are movies are reserved";
+        console.log("There are movies reserved");
+        return "There are movies reserved";
     }
     else{
-        testing();
         console.log("No movies are reserved");
         return "No movies are reserved";
     }
@@ -30,31 +30,10 @@ async function emptyReservationDB(){
 
 
 
-async function testing(){
-    const Res=[];
-    Res.push({
-        email: 'user@example.com',
-        movieName: 'Avatar 2',
-        HallNumber: 1,
-        SeatNumber: 3,
-        Day: 5,
-        Hour: 8
-    });
-    Res.push({
-        email:"adhamez10@gmail.com",
-        movieName:"Black Adam",
-        HallNumber: 2,
-        SeatNumber: 20,
-        Day: 1,
-        Hour: 1
-    })
-    for (let i =0; i<Res.length; i++){
-        MovieReserved.create(Res[i]);
-    }
-}
+
+
 
 module.exports={
     emptyReservationDB:emptyReservationDB,
     AllMovieUser:AllMovieUser,
-    testing:testing
 }
