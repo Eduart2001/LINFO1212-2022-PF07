97260c80ea29456db316a0d92e78f321433f9e67
IsaiZ734 <isaimola@gmail.com>
2022-12-23 17:42:28 +0100

    server-app tests rearrange

--- tests/server-app.test.js
+++ tests/server-app.test.js
@@ -1,6 +1,10 @@
 const request = require("supertest");
 const app = require("../server/server_express");
+const crypto = require('crypto');
+const sequelize = require("../Database/database");
 const server = require("../server/server_express").server;
+const Preferences = require("../Database/Preferences");
+
 
 beforeAll(() => {
   server.close();
@@ -8,6 +12,7 @@ beforeAll(() => {
 afterAll(() => {
   server.close();
 });
+
 describe("Testing login requests", () => {
     it("GET /login", async () => {
         const httpRequest = request(app).get('/login');
@@ -18,13 +23,52 @@ describe("Testing login requests", () => {
         const httpRequest = request(app).get('/login?incorrect=true');
         const response = await httpRequest.expect(200);
         expect(response.status).toBe(200);
-    })
+    });
 });
 
+describe('password change', () => {
+  let oldPassword;
+  let newPassword;
+  let passRep;
 
+  beforeEach(() => {
+    oldPassword = 'adham';
+    newPassword = 'adham1';
+    passRep = 'adham1';
+  });
 
+  afterEach(() => {
+    oldPassword = null;
+    newPassword = null;
+    passRep = null;
+  });
 
-const Preferences = require("../Database/Preferences");
+  it("POST /should not allow old password and new password to be the same", async () => {
+    oldPassword = 'adham';
+    newPassword = 'adham';
+    passRep = 'adham';
+    const httpRequest = request(app).post("/password_change").send({ oldPassword, newPassword, passRep });
+    const response = await httpRequest.expect(200);
+    expect(response.redirects[0]).toInclude('alert=Can%27t%20have%20the%20same%20password%20as%20the%20old%20password');
+  });
+
+  it("POST /should not allow new password and password rep to be different", async () => {
+    newPassword = 'adham1';
+    passRep = 'adham2';
+    const httpRequest = request(app).post("/password_change").send({ oldPassword, newPassword, passRep });
+    const response = await httpRequest.expect(200);
+    expect(response.redirects[0]).toInclude('alert=Retype%20password');
+  });
+
+  it("POST /should update password in database and session when new password is valid", async () => {
+    const httpRequest = request(app).post("/password_change").send({ oldPassword, newPassword, passRep });
+    const response = await httpRequest.expect(200);
+    const result = await sequelize.query(`Select password From Users where email = 'adhamez10@gmail.com'`);
+    const realPass = result[0].password;
+    expect(realPass).toEqual(newPassword);
+    expect(req.session.password).toEqual(newPassword);
+  });
+});
 
 describe('/preferences route', () => {
   it('should update or create a preferences record in the database', async () => {
